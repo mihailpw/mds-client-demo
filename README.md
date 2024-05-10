51c773a6f27d3a9311dd1a5a6ab70b6c4489d202
Mihail Podshivadlov <9678914+mihailpw@users.noreply.github.com>
2024-05-10 17:13:19 +0300

    Add demo;

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# mds-client-demo
+# MDS client demo
+
+From the root folder navigate to project folder and run the demo. Don't forget to add token to the command:
+
+```shell
+cd src/MdsDemo.App && \
+dotnet run -- --token <token_here>
+```
---
+++ src/MdsDemo.App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Headers;
+
+var token = Environment.GetCommandLineArgs().SkipWhile(arg => arg != "--token").Skip(1).FirstOrDefault();
+if (string.IsNullOrEmpty(token))
+    throw new InvalidOperationException("Token was not specified");
+
+var request = new HttpRequestMessage(HttpMethod.Get, "https://api.lyft.com/v2/mds/SFO/trips?end_time=2024-05-01T10");
+request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+request.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/vnd.mds+json;version=2.0"));
+
+using var httpClient = new HttpClient();
+var response = await httpClient.SendAsync(request);
+var jsonString = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(jsonString);
